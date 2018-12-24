35ba231e99102af0423f1a1999f3f1dbe6881126
leo <wanglingsong@gmail.com>
2018-12-24 19:39:58 +0800

    Format code

--- pages/Input_prompt/index.js
+++ pages/Input_prompt/index.js
@@ -1,50 +1,50 @@
 Component({
-    properties:{
-        data: {
-            type: Object,
-            value: {}
-        },
-    },
+  properties: {
     data: {
-        userData: []
+      type: Object,
+      value: {}
     },
-    
-    detached(){
-        this.setData({
-            value: '' 
-        })
+  },
+  data: {
+    userData: []
+  },
+
+  detached() {
+    this.setData({
+      value: ''
+    })
+  },
+
+
+  methods: {
+    getFocus(e) {
+      this.setData({
+        userData: this.data.data.dataArray
+      })
+      this.triggerEvent('onFocus', { name: e.target.dataset.name })
+
     },
-    
 
-    methods: {
-        getFocus(e){
-            this.setData({
-                userData: this.data.data.dataArray
-            })
-            this.triggerEvent('onFocus', {name: e.target.dataset.name})
-            
-        },
-
-        select(e){
-            let dependency = this.data.data.dependency ? this.data.data.dependency : false;
-            this.data.data.dataArray.forEach((v, index) => {
-                if( v.value === e.target.dataset.select.value ){
-                    this.triggerEvent('changeData', { 
-                        id: e.target.dataset.select.id,
-                        index: index,
-                        dependency: dependency,
-                        attr: this.data.data.attr,
-                        value: e.target.dataset.select.value
-                    });
-                }
-            })
-        },
-
-        bindInput(e){
-            // console.log(e.detail.value)
-            this.setData({
-                userData: this.data.data.dataArray.filter(v => v.value.indexOf(e.detail.value) !== -1)
-            })
+    select(e) {
+      let dependency = this.data.data.dependency ? this.data.data.dependency : false;
+      this.data.data.dataArray.forEach((v, index) => {
+        if (v.value === e.target.dataset.select.value) {
+          this.triggerEvent('changeData', {
+            id: e.target.dataset.select.id,
+            index: index,
+            dependency: dependency,
+            attr: this.data.data.attr,
+            value: e.target.dataset.select.value
+          });
         }
+      })
+    },
+
+    bindInput(e) {
+      // console.log(e.detail.value)
+      this.setData({
+        userData: this.data.data.dataArray.filter(v => v.value.indexOf(e.detail.value) !== -1)
+      })
     }
+  }
 })
